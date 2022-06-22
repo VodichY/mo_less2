b5cf8386cf8c61a9556b868acca15ed12d3c998a
VodzichY <yuri.vodich@gmail.com>
2022-06-22 22:55:15 +0300

    fix validation blogger post

--- src/common/validate.ts
+++ src/common/validate.ts
@@ -3,19 +3,6 @@ import  {checkSchema, validationResult } from 'express-validator';
 import { ObjectId } from "mongodb";
 
 const validateBloggersInputModel = checkSchema( {
-	name: {
-		trim: true,
-		notEmpty: {
-			bail: true,
-			errorMessage: 'name field is requered',
-		},
-		isLength: {
-			bail: true,
-			errorMessage: 'name should be not more 15 chars long',
-			options: { max: 15 }
-		}
-
-	},
 	youtubeUrl: {
 		trim: true,
 		notEmpty: {
@@ -32,7 +19,20 @@ const validateBloggersInputModel = checkSchema( {
 			negated: false,
 			errorMessage: 'shortDescription should be URL',
 		}
-	}
+	},
+	name: {
+		trim: true,
+		notEmpty: {
+			bail: true,
+			errorMessage: 'name field is requered',
+		},
+		isLength: {
+			bail: true,
+			errorMessage: 'name should be not more 15 chars long',
+			options: { max: 15 }
+		}
+
+	},
 });
 
 const validatePostInputModel = checkSchema( {
@@ -70,18 +70,19 @@ const validatePostInputModel = checkSchema( {
 			errorMessage: 'content should be not more 1000 chars long',
 			options: { max: 1000 }
 		}
-	}
+	},
 });
 
 const validateHandler = (req: Request, res: Response, next: NextFunction) => {
 	const errors = validationResult(req).formatWith(
 		({ location, msg, param, value, nestedErrors }) => {
 			return { message: msg, field: param };
+			//return `${location}[${param}]: ${msg}`;
 			//{ errorsMessages: [{ message: Any<String>, field: "youtubeUrl" }, { message: Any<String>, field: "name" }], resultCode: 1 }
 		  }
 	);
 	if (!errors.isEmpty()) {
-		res.status(400).json({ errorsMessages: errors.array(), resultCode: 1});
+		res.status(400).json({ errorsMessages: errors.array() });
 	} else {
 		next();
 	};
